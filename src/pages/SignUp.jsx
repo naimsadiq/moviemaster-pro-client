4f9599a40e9775d9bad9c95205eb87a0b43ba38f
naimsadiq <nayemsadiq2013@gmail.com>
2025-11-12 23:33:54 +0600

    Name, Email, Photo URL, Password Buttons: Register, Google Login create done

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -3,11 +3,17 @@ import { FaUser, FaEnvelope, FaLock, FaEyeSlash, FaEye } from "react-icons/fa";
 import { Link, useNavigate } from "react-router";
 import { AuthContext } from "../context/AuthContext";
 import { toast } from "react-toastify";
+import { FcGoogle } from "react-icons/fc";
 
 const SignUp = () => {
   const navigate = useNavigate();
-  const { createUser, updateUserProfile, setUser, setLoading } =
-    useContext(AuthContext);
+  const {
+    createUser,
+    updateUserProfile,
+    setUser,
+    signInWithGoogle,
+    setLoading,
+  } = useContext(AuthContext);
   const [error, setError] = useState("");
   const [errorName, setErrorName] = useState("");
   const [showPassword, setShowPassword] = useState(false);
@@ -16,6 +22,7 @@ const SignUp = () => {
     e.preventDefault();
     setError("");
     const displayName = e.target.name?.value;
+    const photoURL = e.target.photoUrl?.value;
     const email = e.target.email?.value;
     const password = e.target.password?.value;
 
@@ -43,18 +50,31 @@ const SignUp = () => {
       .then((res) => {
         const user = res.user;
         console.log(user);
-        updateUserProfile(displayName)
+        updateUserProfile(displayName, photoURL)
           .then(() => {
             // setUser(user);
             console.log(user);
             navigate("/");
-            // setLoading(false);
+            setLoading(false);
           })
           .catch((error) => {
             console.log(error);
           });
       })
       .catch((e) => {
+        toast.error(e.message);
+      });
+  };
+
+  const handleSignInWithGoogle = () => {
+    signInWithGoogle()
+      .then((res) => {
+        setUser(res.user);
+        navigate(location.state || "/");
+        toast.success("Signin successful");
+      })
+      .catch((e) => {
+        console.log(e);
         toast.error(e.message);
       });
   };
@@ -82,6 +102,24 @@ const SignUp = () => {
                 type="text"
                 name="name"
                 placeholder="Enter your full name"
+                className="w-full py-2 outline-none"
+                required
+              />
+            </div>
+            {errorName && (
+              <p className="text-sm text-red-500 mt-1">{errorName}</p>
+            )}
+          </div>
+          <div>
+            <label className="block text-sm font-medium text-gray-700 mb-1">
+              Photo URL
+            </label>
+            <div className="flex items-center border border-gray-300 rounded-lg px-3">
+              <FaUser className="text-gray-400 mr-2" />
+              <input
+                type="url"
+                name="photoUrl"
+                placeholder="Enter your photo url"
                 className="w-full py-2 outline-none"
                 required
               />
@@ -136,17 +174,25 @@ const SignUp = () => {
           >
             Sign Up
           </button>
-
-          <p className="text-sm text-center text-gray-500 mt-3">
-            Already have an account?{" "}
-            <Link
-              to="/login"
-              className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-pink-500 hover:underline"
-            >
-              Login
-            </Link>
-          </p>
         </form>
+        <div className="mt-5">
+          <button
+            onClick={handleSignInWithGoogle}
+            className="w-full flex items-center justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transition duration-150 ease-in-out cursor-pointer"
+          >
+            <FcGoogle className="h-5 w-5 mr-2" />
+            Google
+          </button>
+        </div>
+        <p className="text-sm text-center text-gray-500 mt-3">
+          Already have an account?{" "}
+          <Link
+            to="/sign-in"
+            className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-pink-500 hover:underline"
+          >
+            Login
+          </Link>
+        </p>
       </div>
     </div>
   );
